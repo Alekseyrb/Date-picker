2121e4848b6ce781508da51cfbba0cfd404dabad
Aleksey <kravchenko.a.a.rb@gmail.com>
2022-07-25 21:43:49 +0300

    Created date picker

---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime _dateTime = DateTime.now();
+
+  void _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    ).then((value) => setState(() {
+      _dateTime = value!;
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              _dateTime.toString(),
+              style: TextStyle(fontSize: 30),
+            ),
+            MaterialButton(
+              onPressed: _showDatePicker,
+              color: Colors.blue,
+              child: const Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'Choose date',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
